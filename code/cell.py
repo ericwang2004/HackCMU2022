3bd15d66a78bd5044c5ca40373949a450d5b7730
Eric Wang <ewang2@erics-mbp-9.wifi.local.cmu.edu>
2022-09-17 10:22:19 -0400

    replace cell

---
+++ code/cell.py
@@ -0,0 +1,13 @@
+# implement cell
+
+class Cell:
+	def __init__(self, x, y, typ):
+		self.x = x
+		self.y = y
+		self.typ = typ
+	def get_x(self):
+		return self.x
+	def get_y(self):
+		return self.y
+	def get_typ(self):
+		return self.typ
