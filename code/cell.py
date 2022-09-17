4d17fda83fab237156fefb30397bc5328ec01d09
Eric Wang <ewang2@erics-mbp-9.wifi.local.cmu.edu>
2022-09-16 22:04:51 -0400

    update cell.py

--- code/cell.py
+++ code/cell.py
@@ -1,1 +1,20 @@
 # cell.py
+
+class Cell:
+	def __init__(self, x, y, typ):
+		'''
+		x, y are coordinates
+		typ represents the type of the cell
+			0 = empty
+			1 = wall
+			2 = magic
+		'''
+		self.x = x
+		self.y = y
+		self.typ = typ
+	def get_x(self):
+		return self.x
+	def get_y(self):
+		return self.y
+	def get_typ(self):
+		return self.typ
