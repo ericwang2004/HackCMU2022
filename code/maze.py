27c3fe11f8934ee88625ec5590360ac92c84d0aa
Stephen <shchien@andrew.cmu.edu>
2022-09-16 22:23:34 -0400

    updating maze

--- code/maze.py
+++ code/maze.py
@@ -1,7 +1,19 @@
 # implement maze
 
+def intArrayIntoCell(input_array):
+    rows, cols = len(input_array), len(input_array[0])
+    cell_array = [0]*rows
+    for i in range(rows):
+        for j in range(cols):
+            array[i][j].x = 
+    return cell_array
+
 class Maze:
-    def __init__(self, ):
+    def __init__(self, x_max, y_max, input_array):
+        self.x_max = x_max
+        self.y_max = y_max
     
-    def get_x():
+    def get_cell_type(self, x, y, input_array):
+        return 
+    
 
