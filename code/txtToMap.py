996aff23bb8678c37cc7b0c8d1adea97dbfe4d50
Stephen <shchien@andrew.cmu.edu>
2022-09-17 10:58:45 -0400

    added new file

--- code/txtToMap.py
+++ code/txtToMap.py
@@ -1,20 +1,36 @@
 # converts .txt output from editor to int array input into Player class
 
-# opens map.txt / lines is an array containing each line as string
-with open('map.txt') as f:
-    lines = f.readlines()
+def txtToInput():
+    # opens map.txt / lines is an array containing each line as string
+    with open('map.txt') as f:
+        lines = f.readlines()
 
-# reads through each line, updating input array
-input_array = []
-for row in rows:
-    input_array.append([0]*cols)
+    def getTuple(s):
+        start = s.find('(')
+        mid = s.find(',')
+        end = s.find(')')
 
-for line in lines:
-    x = 1
-    y = 1
-    typ = 1
-    input_array[x][y] = typ
+        x = int(s[start + 1: mid])
+        y = int(s[mid + 2: end])
+        return x, y
 
-f.close()
-return input_array
+    # get dimensions
+    rows, cols = getTuple(lines[0])
 
+    # reads through each line, updating input array
+    input_array = []
+    for row in range(rows):
+        input_array.append([0]*cols)
+
+    for i in range(1, len(lines)):
+        x, y = getTuple(lines[i])
+        if 'EMPTY' in lines[i]:
+            typ = 0
+        elif 'WALL' in lines[i]:
+            typ = 1
+        elif 'MAGIC' in lines[i]:
+            typ = 2
+        input_array[y][x] = typ
+
+    f.close()
+    return input_array
